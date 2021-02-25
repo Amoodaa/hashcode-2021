e81b9c61de00ee7efb2a6a9b9cf08a29512f0536
Imad Shatali <amoodaa@gmail.com>
2021-02-25 21:23:30 +0200

    reads data

--- src/read.ts
+++ src/read.ts
@@ -1,8 +1,54 @@
 import { readFileSync } from "fs";
+import { Input, Street } from "./types";
 
-const format = (txt: string): Input => {};
+const format = (txt: string): Input => {
+  const [firstLine, ...restOfLines] = txt.split("\n");
+  const [
+    time,
+    intersNo,
+    streetsNo,
+    carsNo,
+    bonusForCarsThatReachEnd,
+  ] = firstLine.split(" ");
+  let i = 0;
+  // parse streets
+  const streets: Record<string, Street> = {};
+  for (i = 0; ; i++) {
+    const line = restOfLines[i];
+    if (!Number.isInteger(+line[line.length - 1])) break;
+    const [startInter, endInter, streetName, streetTime] = line.split(" ");
+    streets[streetName] = {
+      start: +startInter,
+      end: +endInter,
+      stName: streetName,
+      time: +streetTime,
+    };
+  }
+  // parse cars
+  const cars = [];
+  for (; i < restOfLines.length; i++) {
+    const line = restOfLines[i];
+    if (!line) break;
+    const [numberOfStreetsCarWillTake, ...streetsToPass] = line.split(" ");
+    cars.push({
+      streetsNo: +numberOfStreetsCarWillTake,
+      path: streetsToPass,
+    });
+  }
+
+  return {
+    time: +time,
+    intersNo: +intersNo,
+    streetsNo: +streetsNo,
+    carsNo: +carsNo,
+    bonusForCarsThatReachEnd: +bonusForCarsThatReachEnd,
+    cars,
+    streets,
+  };
+};
 
 export const read = (filename: string) => {
   const data = readFileSync(filename);
+  console.log(format(data.toString()));
   return format(data.toString());
 };
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,2 +1,23 @@
-type Input = any;
-type Output = any;
+export type Input = {
+  time: number;
+  intersNo: number;
+  streetsNo: number;
+  carsNo: number;
+  bonusForCarsThatReachEnd: number;
+  streets: Record<string, Street>;
+  cars: Car[];
+};
+
+export type Output = any;
+
+export type Street = {
+  start: number;
+  end: number;
+  stName: string;
+  time: number;
+};
+
+export type Car = {
+  streetsNo: number;
+  path: string[];
+};
